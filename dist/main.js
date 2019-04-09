edc76a805faac9bd219fa550dc9c00e183043d7a
Marty Naselli <mnaselli@worthwhile.com>
2019-04-08 20:29:22 -0400

    got global.hk working

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,4 @@
 const Swagger = require('swagger-client');
 
 const spec = HK_OPEN_API_SPEC;
-const swaggerPromise = Swagger({spec}).then((client) => global.hk = client);
-
-//let HakunaMatata = (path, params={}, config={}) => swaggerPromise.then((client) => {
-//  const errFn = () => console.log(`API: ${path} does not exist`);
-//  return _.get(client, ['apis', ...path], errFn)(params, config);
-//});
+global.hk = Swagger({spec}).client;
