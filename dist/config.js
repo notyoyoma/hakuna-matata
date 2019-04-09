f9f1aa5123e9b7b5f840d1300f499fea9d433e00
Marty Naselli <mnaselli@worthwhile.com>
2019-04-08 20:09:42 -0400

    added sync-request for dev

--- dist/config.js
+++ dist/config.js
@@ -1,5 +1,7 @@
+const fs = require('fs');
 const path = require('path');
 const envGitBranch = require('git-branch');
+const syncRequest = require('sync-request');
 
 const CONFIG_FILE = "hakuna-matata.config.json";
 
@@ -27,19 +29,19 @@ try {
 configFile.push(defaultConfig);
 
 let config;
-try {
-  for (let conf of configFile) {
-    if (
-      (conf.gitBranch === undefined || conf.gitBranch === gitBranch)
-      &&
-      (conf.nodeEnv === undefined || conf.nodeEnv === nodeEnv)
-    ) {
-      config = conf;
-      break;
-    }
+for (let conf of configFile) {
+  if (
+    (conf.gitBranch === undefined || conf.gitBranch === gitBranch)
+    &&
+    (conf.nodeEnv === undefined || conf.nodeEnv === nodeEnv)
+  ) {
+    config = conf;
+    delete config['gitBranch']
+    delete config['nodeEnv']
+    break;
   }
-} catch (error) {
-  console.log('No matching hakuna-matata config found. Continuing with example configuration');
 }
 
+const spec = syncRequest('GET', config.url).getBody().toString('utf8');
+exports.spec = spec;
 exports.default = config;
--- dist/main.js
+++ dist/main.js
@@ -1,12 +1,9 @@
-const config = require('./config.js').default;
 const Swagger = require('swagger-client');
 
+const spec = HK_OPEN_API_SPEC;
+const swaggerPromise = Swagger({spec}).then((client) => global.hk = client);
 
-const swaggerPromise = Swagger(config.url);
-
-const HakunaMatata = (path, params={}, config={}) => swaggerPromise.then((client) => {
-  const errFn = () => console.log(`API: ${path} does not exist`);
-  return _.get(client, ['apis', ...path], errFn)(params, config);
-});
-
-export.default HakunaMatata;
+//let HakunaMatata = (path, params={}, config={}) => swaggerPromise.then((client) => {
+//  const errFn = () => console.log(`API: ${path} does not exist`);
+//  return _.get(client, ['apis', ...path], errFn)(params, config);
+//});
