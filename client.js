9b625db197aba3dec0cae80a8a6de2ba7af35a1a
Marty Naselli <marty.naselli@gmail.com>
2019-05-05 22:28:42 -0400

    switch back to exporting a promise

--- client.js
+++ client.js
@@ -2,14 +2,10 @@ const OpenAPIClientAxios  = require('openapi-client-axios').default;
 
 const definition = JSON.parse(HAKUNA_MATATA_API_SPEC);
 const openApiLib = new OpenAPIClientAxios({definition, validate: false});
-
-async function getApiClient() {
-  return await openApiLib.init();
-}
-const client = getApiClient().client;
+const client = openApiLib.init()
 
 if (process.env.NODE_ENV !== 'production') {
-  global[HAKUNA_MATATA_API_ALIAS] = openApiLib.client;
+  window[HAKUNA_MATATA_API_ALIAS] = client;
 }
 
 module.exports = client;
