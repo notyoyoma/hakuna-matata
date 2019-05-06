5974b2b7f368f2db2cc09f06ad51039b6092bec5
Marty Naselli <marty.naselli@gmail.com>
2019-05-05 21:11:49 -0400

    Revert "swtich from swagger-js to openapi-client-axios"
    This reverts commit 37fc8f3fb8d3c34653171b26a4f81e67dbf265af.

--- client.js
+++ client.js
@@ -1,15 +1,11 @@
-const OpenAPIClientAxios  = require('openapi-client-axios').default;
+const Swagger = require('swagger-client');
 
-const definition = JSON.parse(HAKUNA_MATATA_API_SPEC);
-const openApiLib = new OpenAPIClientAxios({definition, validate: false});
-
-async function getApiClient() {
-  return await openApiLib.init();
-}
-const client = getApiClient().client;
+const spec = JSON.parse(HAKUNA_MATATA_API_SPEC);
+const openApiLib = Swagger({spec});
+const client = openApiLib.client;
 
 if (process.env.NODE_ENV !== 'production') {
-  global[HAKUNA_MATATA_API_ALIAS] = openApiLib.client;
+  global[HAKUNA_MATATA_API_ALIAS] = client;
 }
 
 module.exports = client;
